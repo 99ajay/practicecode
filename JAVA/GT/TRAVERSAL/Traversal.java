47d07f1b295950de81b4fed4300f59cf918e30d6
99ajay <tigersis1995@gmail.com>
2022-10-25 18:16:29 +0530

    preorderGT

---
+++ JAVA/GT/TRAVERSAL/Traversal.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Traversal {
+    public static class Node {
+        int data;
+        ArrayList<Node> children;
+
+        Node(int data) {
+            this.data = data;
+            children = new ArrayList<>();
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Stack<Node> st = new Stack<>();
+        Node root = new Node(arr[0]);
+        st.push(root);
+        int idx = 1;
+        while (idx < arr.length) {
+            Integer val = arr[idx];
+            if (val == null) {
+                st.pop();
+            } else {
+                Node par = st.peek();
+                Node node = new Node(val);
+                par.children.add(node);
+                st.push(node);
+            }
+            idx++;
+        }
+        return root;
+    }
+
+    public static void displaypreorder(Node root) {
+        System.out.print(root.data + " ");
+        for (Node child : root.children) {
+            displaypreorder(child);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer arr[] = { 10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80,
+                null, null, null };
+        Node root = construct(arr);
+        displaypreorder(root);
+    }
+}
